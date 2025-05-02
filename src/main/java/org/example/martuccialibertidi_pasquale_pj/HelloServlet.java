f252956a6843a0e30a98436d95f946b8a4c2cdcd
ant <antonio.aliberti56@studenti.unisa.it>
2025-05-02 18:26:22 +0200

    seconda prova

--- src/main/java/org/example/martuccialibertidi_pasquale_pj/HelloServlet.java
+++ src/main/java/org/example/martuccialibertidi_pasquale_pj/HelloServlet.java
@@ -26,7 +26,4 @@ public class HelloServlet extends HttpServlet {
     public void destroy() {
     }
 
-    public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException{
-        doGet(request, response);
-    }
 }
